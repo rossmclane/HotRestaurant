015d35f97af548fbedcf1cb0909d4cf86e4df8aa
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-11-15 13:12:28 -0500

    working on indivdual delete

--- server.js
+++ server.js
@@ -7,52 +7,63 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Tables Array
-var tables = [
-    {
-        first: "first",
-        second: "second",
-        third: "third",
-        tables: "true"
-    }
-];
+var tables = [{
+    first: "first",
+    second: "second",
+    third: "third",
+    tables: "true"
+}];
 
 // Waitlist Array
-var waitlist = [
-    {
-        first: "first",
-        second: "second",
-        third: "third",
-        waitlist: "true"
-    }
-];
+var waitlist = [{
+    first: "first",
+    second: "second",
+    third: "third",
+    waitlist: "true"
+}];
 
 // home view
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
 // tables view
-app.get('/tables', function(req, res){
+app.get('/tables', function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // reserve view
-app.get('/reserve', function(req, res){
+app.get('/reserve', function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 // tables route
-app.get('/api/tables', function(req, res){
+app.get('/api/tables', function(req, res) {
     res.json(tables);
 });
 
 // waitlist route
-app.get('/api/waitlist', function(req, res){
+app.get('/api/waitlist', function(req, res) {
     res.json(waitlist);
 });
 
+// delete 
+app.delete('/api/tables', function(req, res) {
+    tables = [];
+    res.end();
+})
+
+app.delete('/api/:uniqueID', function(req, res) {
+    const id = req.param.uniqueID;
+    for (let i = 0; i < tables.length; i++) {
+        if (id === tables[i].uniqueId) {
+            tables = tables[i].slice(i, 1)
+        }
+        res.send(tables)
+    }
+})
 
 // Listen
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log(`Listening on Port ${PORT}`);
 });
